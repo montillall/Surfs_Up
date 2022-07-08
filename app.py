3f59b2b4677eac52ce08c5dd25ddec9de5be27c3
montillall <104596428+montillall@users.noreply.github.com>
2022-07-08 11:23:18 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+# 1. Import Flask
+from flask import Flask
+
+# 2. Create an app
+app = Flask(__name__)
+
+# 3. Create Flask routes
+@app.route('/')
+def hello_world():
+    return 'Hello world example'
+
+# 4. Define the about route
+
+
+# 5. Define the contact route
+
+
+# 6. Define main behavior
